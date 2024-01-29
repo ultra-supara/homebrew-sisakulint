6075528b69890b2ee2ab69d64223f2f17e2a3f98
ultra-supara <atsu120225@gmail.com>
2024-01-29 23:06:25 +0900

    update policy controller

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	var rootCmd = &cobra.Command{
+		Use:   "policy-checker",
+		Short: "Policy checker is a CLI tool for checking policies",
+		Long:  `Policy checker is a CLI tool for checking policies based on Open Policy Agent`,
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := policy(); err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		},
+	}
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err.Error()) // PolicyErrorのErrorメソッドを使用
+		os.Exit(1)
+	}
+}
